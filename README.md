083346d46e1ada8a5c00c43525a587286e9384ab
davelijunwei <lijunwei.dave@gmail.com>
2022-11-08 23:09:34 +0800

    code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Project-2
+Project 2 
+
+SEI 25 - Project 2 React App
+![alt text](https://git.generalassemb.ly/bananacognac/iwatch/blob/master/public/Images/Screenshot%202020-11-06%20at%2000.33.31.png)
+
+
+I created a single page application for fast and easy tracking of your personal portfolio without a need to constantly login to your TradingHub.
+
+Users can do the following
+1) Track timeline of stock owned
+2) Track overall % growth
+3) View current prices * not live due to API restriction
+4) Simulate past prices and earnings 
+
+Project WireFrame
+![alt text](https://git.generalassemb.ly/bananacognac/iwatch/blob/master/public/Images/IMG_D0CA02E8334F-1.jpeg)
+
+
+
+Technologies used
+1) REACT.JS
+2) AXIOS
+3) REACT BOOTSTRAP
+4) LIGHT-WEIGHT CHARTS
+5) https://finnhub.io API
---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Portfolio from './components/Portfolio';
+import PortfolioForm from './components/PortfolioForm';
+import './App.css';
+
+function App() {
+  const ALLSTOCKINFOS = localStorage.getItem('stockInfo') ? JSON.parse(localStorage.getItem('stockInfo')) : [];
+  
+  const [stockInfo, setStockInfo] = useState(ALLSTOCKINFOS);
+ 
+  const addStockSymbol = (quote, cost, date) => {    
+    const newStockInfo = [...stockInfo, { quote: quote, cost: Number(cost), date: date, currentHolding: true }];
+    setStockInfo(newStockInfo);
+  };
+
+  const deleteStockSymbol = index => {
+    const newStockInfo = [...stockInfo];
+    newStockInfo.splice(index, 1);
+    setStockInfo(newStockInfo);
+  };
+
+  useEffect(() => {
+  window.localStorage.setItem("stockInfo", JSON.stringify(stockInfo));
+}, [stockInfo]);
+  
+    return (
+      <div>
+        <PortfolioForm addStockSymbol={addStockSymbol} />
+        {stockInfo.map((stock, index) => (
+          <div>
+            
+              <Portfolio
+              
+              key = {stock.id}
+              index = {index}
+              stock={stock}
+              deleteStockSymbol={deleteStockSymbol}
+              
+              /> 
+              
+          </div>           
+        ))}
+      </div>     
+    )
+  }
+
+export default App;
